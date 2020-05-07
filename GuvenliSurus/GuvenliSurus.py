dc20fa15807615dc61b6a10096688187d3194f46
Halil İbrahimi <hikomen@gmail.com>
2020-05-07 16:15:10 +0300

    KameraAc Fonksiyonu eklendi

--- GuvenliSurus/GuvenliSurus.py
+++ GuvenliSurus/GuvenliSurus.py
@@ -1,4 +1,7 @@
 
 #Guvenli Sürüş projesi Teknofest 2020 için hazırladığımız bir projedir.
 #Proje Adı : TRAFİK KOÇU
+
+import kameraAc
 print("Güvenli Sürüş Projemizie Hogeldiniz.")
+kameraAc.kamera()
---
+++ GuvenliSurus/kameraAc.py
@@ -0,0 +1,27 @@
+import cv2
+import sys
+
+
+def kamera():
+        
+
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # kare kare webcam den gelen görüntü yakalanıyor
+        ret, frame = video_capture.read()
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Sonuç ekranda gösteriliyor.
+        cv2.imshow('Video', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # herşey tamamsa ekran yakalaması serbest bırakılıyor.
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+
+
